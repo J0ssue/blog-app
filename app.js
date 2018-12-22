c7a5a7068de750629f7926da89bb7e52b8628be4
J0ssue <jdiaz66@icloud.com>
2018-12-22 14:16:52 +0000

    added mongoose blogs schema and created an entry inside blogs collection also added data dir, and hooked it to mongo,

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const bodyParser = require('body-parser'),// Parser.
        PORT       = 3000,
        app        = express();// starts express.
 
-//  setup
-// mongoose.connect('mongodb://localhost/restful_blog_app');
+// APP CONFIG
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
   created: {type: Date, default: Date.now},
 });
-// title
-// image
-// body
-// created
+const Blog = mongoose.model('Blog', blogSchema);
+
+// RESTFUL ROUTES
+app.get('/blogs', (req, res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => {
   console.log('Server is running in localhost:3000');
